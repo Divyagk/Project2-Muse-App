1d8f172d17bb97a44dd78c2a0d2a2bdf89171c55
divya <gopalakrishnandivya6@gmail.com>
2019-02-21 15:09:33 -0700

    insert datas into js files

---
+++ models/data.js
@@ -0,0 +1,56 @@
+module.exports = function (sequelize, DataTypes) {
+    var Data= sequelize.define("Data", {
+  
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+  },
+  movie_name: {
+    type: DataTypes.STRING(100),
+    allowNull: false
+  },
+  movie_poster: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  movie_genre: {
+    type: DataTypes.STRING(30),
+    allowNull: false
+  },
+  movie_time: {
+    type: DataTypes.STRING(30),
+    allowNull: false
+  },
+  movie_plot: {
+    type: DataTypes.STRING(1000),
+    allowNull: false
+  },
+  movie_director: {
+    type: DataTypes.STRING(100),
+    allowNull: false
+  },
+  movie_actors: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  movie_year: {
+    type: DataTypes.INTEGER(4),
+    allowNull: false
+  },
+  
+  movie_ratingImdb: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  movie_ratingRotten: {
+    type: DataTypes.STRING(30),
+    allowNull: false
+  }
+  });
+  
+    return Data;
+    
+  };
+  
