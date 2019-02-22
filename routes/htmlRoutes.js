603b0059153e466624326f5584a74a8e93cccd7a
divya <gopalakrishnandivya6@gmail.com>
2019-02-22 10:48:11 -0700

    comment added

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,44 +1,31 @@
 var path = require("path");
 
-module.exports = function(app) {
-  // Load index page
-  // app.get("/", function(req, res) {
-  //   db.Example.findAll({}).then(function(dbExamples) {
-  //     res.render("index", {
-  //       msg: "Welcome!",
-  //       examples: dbExamples
-  //     });
-  //   });
-  // });
-  app.get("/", function(req, res) {
+module.exports = function (app) {
+  //  Load index page
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
 
-  // Load example page and pass in an example by id
-  // app.get("/example/:id", function(req, res) {
-  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.render("example", {
-  //       example: dbExample
-  //     });
-  //   });
-  // });
-  app.get("/user", function(req, res) {
+
+  // Load User page
+  app.get("/user", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/user.html"));
   });
-
-  app.get("/emotion", function(req, res) {
+  // Load emotion page here the options such as movies ,books,cookin recipies and games is dispaly to the user.
+  app.get("/emotion", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/emotion.html"));
   });
   // app.get("/entertaiment",function(req, res){
   //   res.sendFile(path.join(__dirname, "../public/entertaiment.html"));
   // });
 
-  app.get("/Games",function(req, res){
+  // Load game page
+  app.get("/Games", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/Games.html"));
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
